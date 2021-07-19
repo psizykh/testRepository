4f656922733535b01987e390c018d0726f331bc9
Pavel <sizykhpavel@gmail.com>
2021-07-19 23:34:26 +0300

    d

---
+++ src/Lesson02/SizykhHW2.java
@@ -0,0 +1,43 @@
+package Lesson02;
+
+public class SizykhHW2 {
+    public static void main(String[] args) {
+        method1(10, 2); // Задание 1 через void
+        System.out.println(method1a(10,5)); // Задание 1 через return
+        method2(0);
+        System.out.println(method3(-5));
+        method4(8, "Pavel");
+
+    }
+
+    public static void method1(int a, int b) {
+        if (a + b >= 10 && a + b <= 20) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+    public static boolean method1a(int a, int b) {
+        return (a + b >= 10 && a + b <= 20);
+    }
+    public static void method2(int a) {
+        if (a >= 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+
+    public static boolean method3(int a) {
+        return  (a <= 0);
+
+    }
+    public static void method4(int a, String stringMethod4) {
+
+        for (int i = 0; i < a; i ++) {
+            System.out.println(stringMethod4 + "" + i);
+
+        }
+
+    }
+}
